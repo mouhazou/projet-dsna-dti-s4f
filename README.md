ff5a08eaa11eed11ba014a07ed50e0074eb019a4
Mouhazou <mouhazou.diarra@regis-dgac.net>
2023-05-22 11:34:40 +0200

    Mise a jour Pour dataset.xml V9.0

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ You can look around to get an idea how to structure your project and, when done,
 [Learn more about git (video).](https://grafikart.fr/formations/git)
 [Learn more about git (article).](https://www.pierre-giraud.com/git-github-apprendre-cours/)
 [How to use several remotes simultaneously] (https://www.nextinpact.com/article/44016/git-comment-travailler-avec-plusieurs-remotes)
+[When and How to user reset, revert and checkout ] (https://www.atlassian.com/fr/git/tutorials/undoing-changes)
+[How to manage projet with git] http://danielkummer.github.io/git-flow-cheatsheet/index.fr_FR.html
--- artas_dataset.py
+++ artas_dataset.py
@@ -34,19 +34,21 @@ def generation_image(polygon_coords , nom):
     m.fit_bounds([polygon_coords])
 
     # Set the temporary file path
-    temp_file = 'html/temp_map.html'
+    temp_file = 'temp_map.html'
     # Save the map to the temporary file
     m.save(temp_file)
 
     # Copy the temporary file to another location
-    shutil.copyfile(temp_file, 'html/temp_map_copy.html')
+    shutil.copyfile(temp_file, 'temp_map_copy.html')
 
     # Set Firefox binary location
     options = Options()
-    options.binary_location = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+    #options.binary_location = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+    options.binary_location = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'  # Remplacez '/chemin/vers/firefox-binary' par le chemin complet du binaire Firefox
     driver = webdriver.Firefox(options=options)
 
-    driver.get('file://' + os.getcwd() + '/html/temp_map_copy.html')
+    #driver.get('file://' + os.getcwd() + '/html/temp_map_copy.html')
+    driver.get('file://' + os.getcwd() + '/temp_map_copy.html')
     # Maximize the browser window
     driver.maximize_window()
     # Wait for 5 seconds to ensure the map is fully loaded
@@ -57,7 +59,95 @@ def generation_image(polygon_coords , nom):
     screenshot = driver.save_screenshot(nom)
     # Close the browser and quit the webdriver
     driver.quit()
+def menu(xml_dico):
+    zone = -1
+    while zone < 0 or zone > 9:
+        print(
+          "- 0 => Quitter\n"
+          "- 1 => ENRTAs\n"
+          "- 2 => TMAs\n"
+          "- 3 => SIDs\n"
+          "- 4 => STARs\n"
+          "- 5 => TRAs\n"
+          "- 6 => MAs\n"
+          "- 7 => Air Routes\n"
+          "- 8 => ICCAs\n"
+          "- 9 => All\n")
+        try :
+            zone = int(input("Veuillez choisir le numero de la zone a tracer ou quittez (0):\n"))
+        except:
+            print("Erreur : Veuillez saisir un chiffre compris entre 0 et 9 correspondant a la zone SVP")
+    match zone:
+        case 0:
+             exit ()
+        case 1:
+            traitement_enrta (xml_dico)
+        case 2:
+            traitement_tma (xml_dico)
+        case 3:
+            traitement_sid (xml_dico)
+        case 4:
+            traitement_star (xml_dico)
+        case 5:
+            traitement_tra (xml_dico)
+        case 6:
+            traitement_ma (xml_dico)
+        case 7:
+            traitement_air (xml_dico)
+        case 8:
+            traitement_icca (xml_dico)
+        case 9:
+            traitement_all (xml_dico)
+        
+def  traitement_enrta (xml_dico):
+    enrta_list = xml_dico['dataset']['enrta_main']['enrta']
+    if isinstance(enrta_list, dict):  # Vérifie si c'est un dictionnaire
+        enrta_list = [enrta_list]  # Convertit en une liste avec un seul élément
+    for enrta in enrta_list:
+        coodinates =[]
+        for pos in enrta['area']['pos']:
+            lat = float(pos['lat'])
+            lon = float(pos['lon'])
+            coodinates.append((lat, lon))
+        generation_image (coodinates, enrta['name'] + '.png')
+    return 0 
 
+def traitement_tma (xml_dico):
+    enrta_list = xml_dico['dataset']['tma_main']['tma']
+    if isinstance(enrta_list, dict):  # Vérifie si c'est un dictionnaire
+        enrta_list = [enrta_list]  # Convertit en une liste avec un seul élément
+    for tma in enrta_list:
+        coodinates =[]
+        for pos in tma['area']['pos']:
+            lat = float(pos['lat'])
+            lon = float(pos['lon'])
+            coodinates.append((lat, lon))
+        generation_image (coodinates, tma['name'] + '.png')
+    return 0
+
+def traitement_sid (xml_dico):
+    sid_list = xml_dico['dataset']['sid_main']['sid']
+    if isinstance(sid_list, dict):  # Vérifie si c'est un dictionnaire
+        sid_list = [sid_list]  # Convertit en une liste avec un seul élément
+    for sid in sid_list:
+        coodinates =[]
+        for pos in sid['area']['pos']:
+            lat = float(pos['lat'])
+            lon = float(pos['lon'])
+            coodinates.append((lat, lon))
+        generation_image (coodinates, sid['name'] + '.png')
+    return 0
+
+def traitement_star (xml_dico):
+    return 0
+def traitement_tra (xml_dico):
+    return 0
+def traitement_ma (xml_dico):
+    return 0
+def traitement_air (xml_dico):
+    return 0
+def  traitement_all (xml_dico):
+    return 0
 def traitement_icca (xml_dico):
     for icca in xml_dico['dataset']['icca_main']['icca']:
         print(icca['name'])
@@ -71,24 +161,39 @@ def traitement_icca (xml_dico):
 
 
 def traitement_doi(xml_dico):
+    i=0
     for doi in xml_dico['dataset']['unit_doi_main']['unit_doi']:
         #print(doi['name'])
         coodinates =[]
-        for pos in doi:
-            print (pos)
-            lat = float(pos['lat'])
-            lon = float(pos['lon'])
-            coodinates.append((lat, lon))
+    
+        i=i+1
+        print (doi)
+        dir = 1 if "doi_long_e" in doi else -1
+        lat = dms_to_deg(float(doi['doi_lat_deg']),float(doi['doi_lat_min']),float(doi['doi_lat_sec']),float(doi['doi_lat_n']))
+        lon = dms_to_deg(float(doi['doi_long_deg']),float(doi['doi_long_min']),float(doi['doi_long_sec']),dir)
+        coodinates.append((lat, lon))
         print (coodinates)
-        generation_image (coodinates, 'doi.png')
+        generation_image (coodinates, 'doi_{}.png'.format(i))
+
+def dms_to_deg(degrees, minutes, seconds,dir):
+    # Calcul du degré décimal
+    dd = degrees + (minutes / 60) + (seconds / 3600)
+    if (dir):
+        return dd
+    else:
+        return -dd
 
 def main():
-    with open('Artas/4F_O0502.xml', 'r') as file:
+    data="4F_E0209.xml"
+    #data='4F_O0502.xml'
+    with open(data, 'r') as file:
         xml_string = file.read()
         
-    xml_dict = xmltodict.parse(xml_string)
-    #traitement_icca(xml_dict)
-    traitement_doi(xml_dict)
+    xml_dico = xmltodict.parse(xml_string)
+    
+    menu (xml_dico)
+    #traitement_icca(xml_dico)
+    #traitement_doi(xml_dict)
     #print (xml_dict)
     
 
